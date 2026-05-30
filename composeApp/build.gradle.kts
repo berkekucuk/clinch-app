5fb095286c2c524f4e8c2e9aca494ed9037268ce
berkekucuk <berke653.bk@gmail.com>
2026-05-31 00:28:10 +0300

    chore: bump app version to 1.3.3 and update build numbers

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -131,8 +131,8 @@ android {
         applicationId = "com.berkekucuk.mmaapp"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 6
-        versionName = "1.3.2"
+        versionCode = 7
+        versionName = "1.3.3"
     }
     packaging {
         resources {

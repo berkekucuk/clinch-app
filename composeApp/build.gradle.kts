fea771bfeaa51cfddc3ded1ca3c90ebb45da6a30
berkekucuk <berke653.bk@gmail.com>
2026-06-11 23:23:24 +0300

    chore: bump application version to 1.4.0 and update build codes

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -131,8 +131,8 @@ android {
         applicationId = "com.berkekucuk.mmaapp"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 14
-        versionName = "1.3.9"
+        versionCode = 15
+        versionName = "1.4.0"
     }
     packaging {
         resources {

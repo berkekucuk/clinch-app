c99479e273b951e63abd91eb8462b388297b9c84
berkekucuk <berke653.bk@gmail.com>
2026-07-18 20:04:47 +0300

    refactor: apply @Immutable annotations and convert computed properties to stable values in domain models

--- composeApp/src/commonMain/kotlin/com/berkekucuk/mmaapp/domain/model/AppConfig.kt
+++ composeApp/src/commonMain/kotlin/com/berkekucuk/mmaapp/domain/model/AppConfig.kt
@@ -1,5 +1,8 @@
 package com.berkekucuk.mmaapp.domain.model
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 data class AppConfig(
     val key: String,
     val valueEn: String,
--- composeApp/src/commonMain/kotlin/com/berkekucuk/mmaapp/domain/model/WeightClass.kt
+++ composeApp/src/commonMain/kotlin/com/berkekucuk/mmaapp/domain/model/WeightClass.kt
@@ -1,9 +1,12 @@
 package com.berkekucuk.mmaapp.domain.model
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 data class WeightClass(
     val id: String,
     val sortOrder: Int,
     val rankings: List<RankedFighter>
 ) {
-    val isWomens: Boolean get() = id in setOf("womens_p4p", "SW", "W_FLW", "W_BW")
+    val isWomens: Boolean = id in setOf("womens_p4p", "SW", "W_FLW", "W_BW")
 }
